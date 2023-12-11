2a2f9cebdb2c21eb0f997461d721b5a940edd2a2
Reka Ferenczy <ferenczy.reka01@gmail.com>
2023-12-11 17:00:45 +0100

    Add files for Registration endpoint

---
+++ src/main/java/com/greenfoxacademy/springwebapp/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.springwebapp.repositories;
+
+import com.greenfoxacademy.springwebapp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    Optional<User> findUserByEmail(String email);
+}
