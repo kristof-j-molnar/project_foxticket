8bcd7d286d7cee1706ab6c21a4e890804a516b99
abeth-98 <gyorijulia25@gmail.com>
2023-12-14 09:53:09 +0100

    Resolve conflicts about rebase

---
+++ src/main/java/com/greenfoxacademy/springwebapp/services/ProductServiceImp.java
@@ -0,0 +1,30 @@
+package com.greenfoxacademy.springwebapp.services;
+
+import com.greenfoxacademy.springwebapp.dtos.ProductDTO;
+import com.greenfoxacademy.springwebapp.dtos.ProductListResponseDTO;
+import com.greenfoxacademy.springwebapp.models.Product;
+import com.greenfoxacademy.springwebapp.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImp implements ProductService {
+
+  private ProductRepository productRepository;
+
+  @Autowired
+  public ProductServiceImp(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+  public ProductListResponseDTO getAvailableProductsInDTO() {
+    List<Product> productList = productRepository.findAll();
+    ProductListResponseDTO productDTOs = new ProductListResponseDTO();
+    for (Product product : productList) {
+      productDTOs.add(new ProductDTO(product.getId(), product.getName(), product.getPrice(), product.getDuration(), product.getDescription(), product.getType().getName()));
+    }
+    return productDTOs;
+  }
+}
