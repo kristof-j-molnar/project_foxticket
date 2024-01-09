8ca852f7a47c13032671814dc0a4ff9698542808
Reka Ferenczy <ferenczy.reka01@gmail.com>
2024-01-09 14:40:26 +0100

    Add codes for password encoder

--- src/main/java/com/greenfoxacademy/springwebapp/BasicSpringProjectApplication.java
+++ src/main/java/com/greenfoxacademy/springwebapp/BasicSpringProjectApplication.java
@@ -9,16 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BasicSpringProjectApplication implements CommandLineRunner {
   private UserRepository userRepository;
   private ProductTypeRepository productTypeRepository;
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
 
   @Autowired
-  public BasicSpringProjectApplication(UserRepository userRepository, ProductTypeRepository productTypeRepository) {
+  public BasicSpringProjectApplication(UserRepository userRepository, ProductTypeRepository productTypeRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
     this.userRepository = userRepository;
     this.productTypeRepository = productTypeRepository;
+    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
   }
 
   public static void main(String[] args) {
@@ -27,9 +30,9 @@ public class BasicSpringProjectApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    User user1 = new User("reka", "ferenczy.reka01@gmail.com", "reka12345", "USER");
+    User user1 = new User("reka", "ferenczy.reka01@gmail.com", bCryptPasswordEncoder.encode("reka12345"), "USER");
     userRepository.save(user1);
-    User user = new User("user", "lacika.com", "pass", "User");
+    User user = new User("user", "lacika.com", bCryptPasswordEncoder.encode( "pass"), "User");
     userRepository.save(user);
 
     Product p1 = new Product("Vonaljegy", 480, 90, "90 perces vonaljegy BP-n!");
