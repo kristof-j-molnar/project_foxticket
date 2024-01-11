9d7e35ec1a66449380c0d8eb96d4fe49efb18261
Reka Ferenczy <ferenczy.reka01@gmail.com>
2024-01-11 14:55:12 +0100

    Add modified admin role codes

--- src/main/java/com/greenfoxacademy/springwebapp/security/SecurityConfigurer.java
+++ src/main/java/com/greenfoxacademy/springwebapp/security/SecurityConfigurer.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -18,9 +20,12 @@ public class SecurityConfigurer {
 
   private JwtRequestFilter jwtRequestFilter;
 
+  private AccessDeniedHandler accessDeniedHandler;
+
   @Autowired
-  public SecurityConfigurer(JwtRequestFilter jwtRequestFilter) {
+  public SecurityConfigurer(JwtRequestFilter jwtRequestFilter, AccessDeniedHandler accessDeniedHandler) {
     this.jwtRequestFilter = jwtRequestFilter;
+    this.accessDeniedHandler = accessDeniedHandler;
   }
 
   @Bean
@@ -30,20 +35,31 @@ public class SecurityConfigurer {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.ignoringRequestMatchers("/api/users/**", "/api/news", "/api/hello"));
 
-    http.csrf(csrf -> csrf.ignoringRequestMatchers("/api/users/**", "/api/news", "/api/hello", "/api/cart"));
     http.authorizeHttpRequests(authorize -> authorize
-        .requestMatchers("/admin").hasRole("ADMIN")
-        .requestMatchers("/api/users/**", "/api/news", "/api/cart")
-        .permitAll()
-        .anyRequest()
-        .authenticated()).httpBasic(basic -> basic.authenticationEntryPoint(((request, response, authException) -> response.sendError(HttpStatus.UNAUTHORIZED.value(), "no-no"))));
+        .requestMatchers("/api/admin").hasRole("ADMIN")
+        .requestMatchers("/api/users/**", "/api/news", "/api/cart", "/api/products")
+        .permitAll());
+
+    http.exceptionHandling(
+        customizer -> customizer.accessDeniedHandler(((request, response, accessDeniedException) -> {
+          response.setStatus(HttpStatus.FORBIDDEN.value());
+          response.getWriter().write("Unauthorized access");
+        })));
+
     http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
     return http.build();
   }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AccessDeniedHandler accessDeniedHandler() {
+    return new CustomAccessDeniedHandler();
   }
 }
