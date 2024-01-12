7b6cc86525f1cab6ea53a6aeea6dafa2f067ff3b
Reka Ferenczy <ferenczy.reka01@gmail.com>
2024-01-12 10:57:54 +0100

    Add admin role codes after mentor time

--- src/main/java/com/greenfoxacademy/springwebapp/services/UserAuthenticationServiceImpl.java
+++ src/main/java/com/greenfoxacademy/springwebapp/services/UserAuthenticationServiceImpl.java
@@ -1,6 +1,5 @@
 package com.greenfoxacademy.springwebapp.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +10,7 @@ public class UserAuthenticationServiceImpl implements UserAuthenticationService 
   }
 
   public boolean hasRole(Authentication authentication){
-      if (authentication.getAuthorities().stream().noneMatch(a -> a.getAuthority().equals("ADMIN"))){
+      if (authentication.getAuthorities().stream().noneMatch(a -> a.getAuthority().equalsIgnoreCase("admin"))){
         return true;
       } else {
         return false;
