db19575d4b3f333883167fd5813c22679692e6ec
Fridel Istvan <istvan.fridel@gmail.com>
2024-01-18 09:57:21 +0100

    delete unused code

--- src/main/java/com/greenfoxacademy/springwebapp/controllers/ProductController.java
+++ src/main/java/com/greenfoxacademy/springwebapp/controllers/ProductController.java
@@ -5,8 +5,6 @@ import com.greenfoxacademy.springwebapp.services.ProductService;
 import com.greenfoxacademy.springwebapp.services.ProductServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +22,6 @@ public class ProductController {
 
   @RequestMapping(path = "/products", method = RequestMethod.GET)
   public ResponseEntity<ProductListResponseDTO> getAvailableProducts() {
-    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-    Object principal = auth.getPrincipal();
     return ResponseEntity.status(200).body(productService.getAvailableProductsInDTO());
   }
 }
