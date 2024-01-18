1257515f645ab30a98f1e53b7a90845d045d28ad
Fridel Istvan <istvan.fridel@gmail.com>
2024-01-18 10:56:50 +0100

    annotate EditProfileAPITest with @Transactional

--- src/test/java/com/greenfoxacademy/springwebapp/integration/EditProfileAPITest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/integration/EditProfileAPITest.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.springwebapp.integration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.greenfoxacademy.springwebapp.dtos.EditProfileDTO;
 import com.greenfoxacademy.springwebapp.dtos.UserLoginDTO;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Transactional
 public class EditProfileAPITest {
 
   @Autowired
