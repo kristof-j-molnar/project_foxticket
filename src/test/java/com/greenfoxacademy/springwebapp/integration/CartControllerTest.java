822cf02f981db594d5afdd6eb5ebbe9996e289e8
abeth-98 <gyorijulia25@gmail.com>
2024-01-25 10:06:25 +0100

    Remove logger

--- src/test/java/com/greenfoxacademy/springwebapp/integration/CartControllerTest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/integration/CartControllerTest.java
@@ -5,8 +5,6 @@ import com.greenfoxacademy.springwebapp.dtos.ProductAddingRequestDTO;
 import com.greenfoxacademy.springwebapp.dtos.UserLoginDTO;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,8 +30,6 @@ class CartControllerTest {
   MockMvc mvc;
 
   ObjectMapper objectMapper = new ObjectMapper();
-
-  Logger log = LoggerFactory.getLogger(getClass());
 
   @Test
   void getProductsInCart_ReturnAListAnd200() throws Exception {
