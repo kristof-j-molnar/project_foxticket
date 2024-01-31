bd506a8c2457082ceca3d460cd0d0d6a0fa1fd84
Kristóf Molnár <kristof.j.molnar@gmail.com>
2024-01-31 09:18:22 +0100

    change name of cartitems table

--- src/main/java/com/greenfoxacademy/springwebapp/models/CartItem.java
+++ src/main/java/com/greenfoxacademy/springwebapp/models/CartItem.java
@@ -2,8 +2,7 @@ package com.greenfoxacademy.springwebapp.models;
 
 import jakarta.persistence.*;
 
-@Entity
-@Table(name = "cartItems")
+@Entity(name = "cartitems")
 public class CartItem {
 
   @Id
